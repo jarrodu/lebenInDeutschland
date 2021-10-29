0316e9eea00cf56f77bfc258d12702460cb3ca0a
AyberkAhmet <ayberkahmet2104@gmail.com>
2021-10-29 21:44:36 +0300

    questions page added

--- lib/screens/all_questions.dart
+++ lib/screens/all_questions.dart
@@ -1,34 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:leben_in_deutschland/models/question_model.dart';
 import 'package:leben_in_deutschland/viewModels/question_view_model.dart';
 import 'package:provider/provider.dart';
 
-class AllQuestions extends StatelessWidget {
+class AllQuestions extends StatefulWidget {
   const AllQuestions({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final _questionsViewModel = Provider.of<QuestionViewModel>(context);
-    final controller = PageController();
+  State<AllQuestions> createState() => _AllQuestionsState();
+}
 
+class _AllQuestionsState extends State<AllQuestions> {
+  final controller = PageController();
+  bool? isTrue;
+  List<Color> optionColors = [
+    Colors.black,
+    Colors.black,
+    Colors.black,
+    Colors.black,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final QuestionViewModel _questionsViewModel = Provider.of<QuestionViewModel>(context);
+    final List<QuestionModel> questions = _questionsViewModel.questions;
 
     return Scaffold(
       appBar: AppBar(),
       body: Center(
         child: PageView(
+          clipBehavior: Clip.antiAlias,
+          onPageChanged: (value) {
+            optionColors = [
+              Colors.black,
+              Colors.black,
+              Colors.black,
+              Colors.black,
+            ];
+            setState(() {
+              
+            });
+          },
           controller: controller,
-          children: List.generate(_questionsViewModel.questions.length, (index) {
+          children: List.generate(questions.length, (questionIndex) {
             return Center(
               child: Column(
                 children: [
-                  Text(_questionsViewModel.questions[index].question),
-                  Column(
-                    children: [
-                      ElevatedButton(onPressed: null, child: Text(_questionsViewModel.questions[index].options[0])),
-                      ElevatedButton(onPressed: null, child: Text(_questionsViewModel.questions[index].options[1])),
-                      ElevatedButton(onPressed: null, child: Text(_questionsViewModel.questions[index].options[2])),
-                      ElevatedButton(onPressed: null, child: Text(_questionsViewModel.questions[index].options[3])),
-                    ],
-                  ),
+                  _buildQuestionText(questionIndex, questions),
+                  //TODO image
+                  _buildQuestionOptions(questionIndex, questions),
                 ],
               ),
             );
@@ -36,5 +56,33 @@ class AllQuestions extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Text _buildQuestionText(int questionIndex, List<QuestionModel> questions) {
+    return Text((questionIndex + 1).toString() + ". " + questions[questionIndex].question);
+  }
+
+  Column _buildQuestionOptions(int questionIndex, List<QuestionModel> questions) {
+    return Column(
+        children: List.generate(
+      4,
+      (optionIndex) => ElevatedButton(
+          style: ElevatedButton.styleFrom(primary: optionColors[optionIndex]),
+          onPressed: () => controlQuestion(questions, optionIndex, questionIndex),
+          child: Text(questions[questionIndex].options[optionIndex])),
+    ));
+  }
+
+  void controlQuestion(List<QuestionModel> questions, int optionIndex, int questionIndex) {
+    if (questions[questionIndex].options[optionIndex] == questions[questionIndex].correctAnswer) {
+      isTrue = true;
+      optionColors[optionIndex] = Colors.green;
+      setState(() {});
+    } else {
+      isTrue = false;
+      optionColors[optionIndex] = Colors.red;
+      setState(() {});
+    }
+    print(isTrue);
   }
 }
